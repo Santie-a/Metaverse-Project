4b1df9e28697dc85b749d76df1db80235885483b
Santie-a <diaz.obando.santiago@gmail.com>
2022-10-02 16:19:37 -0500

    Second Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -56,35 +56,34 @@ export default function Header() {
                                     </svg>
                                 </Link>
 
-                                <button
-                                    type="button"
-                                    title="Sign Out"
+                                <Link
+                                    to={ROUTES.LOGIN}
+                                    aria-label="login"
                                     onClick={() => {
-                                        firebase.auth().signOut()
-                                        window.location.reload()
-                                    }}
-                                    onKeyDown={(event) => {
-                                        if (event.keyCode === 'Enter') {
                                             firebase.auth().signOut()
-                                        }
-                                        window.location.reload()
-                                    }}
+                                        }}
+                                    onKeyDown={(event) => {
+                                            if (event.keyCode === 'Enter') {
+                                                firebase.auth().signOut()
+                                            }
+                                            window.location.reload()
+                                        }}
                                 >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke-width="1.5"
-                                        stroke="currentColor"
-                                        className="w-8 h-6 mr-6 hover:opacity-40 ease-in duration-100"
-                                    >
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-                                        />
-                                    </svg>
-                                </button>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            className="w-8 h-6 mr-6 hover:opacity-40 ease-in duration-100"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                                            />
+                                        </svg>
+                                </Link>
                                 <div className="flex items-center cursor-pointer">
                                     <Link to={`/p/${user.displayName}`}>
                                         <svg
--- src/pages/buy-nft.js
+++ src/pages/buy-nft.js
@@ -7,9 +7,12 @@ import teamsData from "../data/teamsData.json"
 export default function BuyNFT() {
     const [ error, setError ] = useState("")
     const [ selection, setSelection ] = useState("")
+    const [ currencies, setCurrencies ] = useState(500)
     const [ teams, setTeams ] = useState(teamsData)
+    const [ finalTeam, setFinalTeam ] = useState("")
+    
     const { user } = useContext(UserContext)
-    const isInvalid = user ? (false) : (true) || selection === ""
+    const [ isInvalid, setisInvalid ] = useState(user ? (false) : (true) || selection === "")
 
     useEffect(() => {
         document.title = 'Metaverse Project - Buy NFT'
@@ -29,15 +32,22 @@ export default function BuyNFT() {
     }
 
     function processBuy() {
-        console.log("buying")
+        console.log("buying", teams[selection - 1].name)
+        setCurrencies(prevCurrencies => {
+            return prevCurrencies - teams[selection - 1].price
+        })
+        setFinalTeam(selection)
     }
 
     function handleBuy() {
-        selection === 0 || selection === "" ?
-            setError("Select One Team") : 
+        if (selection === 0 || selection === "") {
+            setError("Select One Team")
+        } else { 
             processBuy()
             setError("")
+            setisInvalid(true)
         }
+    }
 
     const teamsRender = teams.map(item => {
         return (
@@ -45,6 +55,7 @@ export default function BuyNFT() {
                 img={item.img}
                 name={item.name}
                 id={item.id}
+                price={item.price}
                 selected={item.selected}
                 toggle={() => toggle(item.id)}
             />
@@ -54,12 +65,35 @@ export default function BuyNFT() {
     return (
         <div className="bg-gray-background">
             <Header />
-            <div className="container flex mx-auto max-w-screen-md justify-center items-center h-auto flex-col">
-                <h1 className="text-2xl font-bold text-gray-base mb-4">BUY YOUR TEAM NFT</h1>
+            <div
+                className="container flex mx-auto max-w-screen-md justify-center items-center h-auto flex-col"
+            >
+                <h1 className="text-2xl font-bold text-gray-background mb-4">BUY YOUR TEAM NFT</h1>
+                <div className="flex bg-white justify-center items-center border border-gray-primary rounded p-4 mb-4">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-8 h-8 gray-base mr-3"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
+                        />
+                    </svg>
+                    <h2
+                        className="text-2xl font-bold text-gray-base"
+                    >
+                        MSC: {currencies}
+                    </h2>
+                </div>
                 <div className="grid grid-cols-3 grid-flow-row">
                     {teamsRender}
                 </div>
-                <p className="mb-4 text-xs text-red-primary">{error}</p>
+                {error && <p className="m-2 text-xs text-red-primary">Error: {error}</p>}
                 <button
                     className={
                     `m-2 bg-blue-medium text-white w-8/12 rounded h-8 font-medium ease-in duration-300
@@ -71,6 +105,29 @@ export default function BuyNFT() {
                     Buy NFT
                 </button>
             </div>
+            {finalTeam &&
+                <div className="container bg-white flex mx-auto max-w-screen-md justify-center items-center h-auto flex-col border border-gray-primary rounded p-5">
+                    <div className="flex p-4 border border-gray-primary rounded">
+                        <h1 className="text-2xl font-bold text-gray-base">Operation Successful!</h1>
+                    </div>
+                    <div className="flex m-2 p-2 items-center justify-center">
+                        <div className="m-2 p-5 w-6/12 border border-gray-primary rounded">
+                            <img
+                                src={teams[finalTeam - 1].img}
+                                alt="Shield"
+                                className=""
+                            />
+                        </div>
+                        <div className="flex flex-col w-6/12 items-center justify-center border m-2 p-5 border-gray-primary rounded">
+                            <h1 className="text-2xl font-bold">Team</h1>
+                            <p className="text-2xl justify-center">{teams[finalTeam - 1].name}</p>
+                            <h1 className="text-2xl font-bold mt-2">Price</h1>
+                            <p className="text-2xl">{teams[finalTeam - 1].price} MSC</p>
+                            <p className="text-xl">Now you have your own team! Go to the metasoccer section</p>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
